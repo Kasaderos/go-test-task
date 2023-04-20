d412ec2ff5c4e20dba9712d2cfba787b0f415341
Berdibek <BKaldarov@one.kz>
2023-04-20 17:14:56 +0600

    add worker pool
    - add shutdown by signal

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
+	"os/signal"
 	"sync"
+	"syscall"
 	"time"
 )
 
@@ -15,21 +19,49 @@ type Data struct {
 	B int64 `json:"b"`
 }
 
-func sumBlock(data []Data, start, end int, resultC chan<- int64) {
-	// data read-only
+type Job struct {
+	Data  []Data
+	Start int
+	End   int
+}
+
+func sumBlock(data []Data, start, end int) int64 {
 	sum := int64(0)
 	for i := start; i < end; i++ {
 		sum += data[i].A + data[i].B
 	}
-	resultC <- sum
+	return sum
+}
+
+func worker(ctx context.Context, jobC <-chan Job, resultC chan<- int64, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case job, ok := <-jobC:
+			if !ok {
+				return
+			}
+			sum := sumBlock(job.Data, job.Start, job.End)
+			resultC <- sum
+		}
+	}
 }
 
 func main() {
 	var numWorkers int
+	var jobQueueSize int
+	var fileName string
 	flag.IntVar(&numWorkers, "n", 4, "number of workers")
+	flag.IntVar(&jobQueueSize, "q", 4, "job queue size")
+	flag.StringVar(&fileName, "f", "data.json", "number of workers")
 	flag.Parse()
 
-	bytes, err := ioutil.ReadFile("data.json")
+	ctx, cancelCtx := context.WithCancel(context.Background())
+	go initGracefulShutDown(cancelCtx)
+
+	bytes, err := ioutil.ReadFile(fileName)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -40,32 +72,52 @@ func main() {
 	}
 
 	t := time.Now()
-	ch := make(chan int64)
+	resultC := make(chan int64)
+	jobC := make(chan Job, jobQueueSize)
 
-	blockSize := 100
 	var wg sync.WaitGroup
-	for i := 0; i < len(data); i += blockSize {
-		start := i
-		end := i + blockSize
-		if end > len(data) {
-			end = len(data)
-		}
+	for i := 0; i < numWorkers; i++ {
 		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			sumBlock(data, start, end, ch)
-		}()
+		go worker(ctx, jobC, resultC, &wg)
 	}
 
+	// send blocks
 	go func() {
+		blockSize := 100
+	LP:
+		for i := 0; i < len(data); i += blockSize {
+			start := i
+			end := i + blockSize
+			if end > len(data) {
+				end = len(data)
+			}
+			job := Job{
+				Data:  data,
+				Start: start,
+				End:   end,
+			}
+			select {
+			case <-ctx.Done():
+				break LP
+			case jobC <- job:
+			}
+		}
+		close(jobC)
+
 		wg.Wait()
-		close(ch)
+		close(resultC)
 	}()
 
 	sum := int64(0)
-	for s := range ch {
+	for s := range resultC {
 		sum += s
 	}
-
 	fmt.Println("Sum:", sum, "Time:", time.Since(t))
+}
+
+func initGracefulShutDown(cancel context.CancelFunc) {
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+	log.Println("graceful shutdown", <-sigChan)
+	cancel()
 }
--- main_atomic.go
+++ main_atomic.go
@@ -1,36 +1,68 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
+	"os/signal"
 	"sync"
 	"sync/atomic"
+	"syscall"
 	"time"
 )
+
+var JobQueueSize = 4
 
 type Data struct {
 	A int64 `json:"a"`
 	B int64 `json:"b"`
 }
 
-func sumBlock(data []Data, start, end int, result *int64) {
-	// data read-only
+type Job struct {
+	Data  []Data
+	Start int
+	End   int
+}
+
+func sumBlock(data []Data, start, end int) int64 {
 	sum := int64(0)
 	for i := start; i < end; i++ {
 		sum += data[i].A + data[i].B
 	}
-	atomic.AddInt64(result, sum)
+	return sum
+}
+
+func worker(ctx context.Context, jobC <-chan Job, result *int64, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case job, ok := <-jobC:
+			if !ok {
+				return
+			}
+			sum := sumBlock(job.Data, job.Start, job.End)
+			atomic.AddInt64(result, sum)
+		}
+	}
 }
 
 func main() {
 	var numWorkers int
+	var fileName string
 	flag.IntVar(&numWorkers, "n", 4, "number of workers")
+	flag.StringVar(&fileName, "f", "data.json", "number of workers")
 	flag.Parse()
 
-	bytes, err := ioutil.ReadFile("data.json")
+	ctx, cancelCtx := context.WithCancel(context.Background())
+	go initGracefulShutDown(cancelCtx)
+
+	bytes, err := ioutil.ReadFile(fileName)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -41,23 +73,43 @@ func main() {
 	}
 
 	t := time.Now()
+	jobC := make(chan Job, JobQueueSize)
+
 	sum := int64(0)
+	var wg sync.WaitGroup
+	for i := 0; i < numWorkers; i++ {
+		wg.Add(1)
+		go worker(ctx, jobC, &sum, &wg)
+	}
 
 	blockSize := 100
-	var wg sync.WaitGroup
+LP:
 	for i := 0; i < len(data); i += blockSize {
 		start := i
 		end := i + blockSize
 		if end > len(data) {
 			end = len(data)
 		}
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			sumBlock(data, start, end, &sum)
-		}()
+		job := Job{
+			Data:  data,
+			Start: start,
+			End:   end,
+		}
+		select {
+		case <-ctx.Done():
+			break LP
+		case jobC <- job:
+		}
 	}
-
+	close(jobC)
 	wg.Wait()
+
 	fmt.Println("Sum:", sum, "Time:", time.Since(t))
+}
+
+func initGracefulShutDown(cancel context.CancelFunc) {
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+	log.Println("graceful shutdown", <-sigChan)
+	cancel()
 }
